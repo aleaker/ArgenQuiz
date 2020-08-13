984778c0651e3d8fc74ba6e223e67112e09fe9c0
aleaker <alejoakerfeld@gmail.com>
2020-08-13 15:37:31 -0300

    questionsTable created on dynamoDB

--- quiz-api/src/handlers/getAnswers.js
+++ quiz-api/src/handlers/getAnswers.js
@@ -1,9 +1,9 @@
 async function getAnswers(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "hi there!" }),
+    body: JSON.stringify({ message: "hi there!" }), //resp body must be a string
   };
-}
+};
 
 export const handler = getAnswers;
 
