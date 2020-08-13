7a1358ac095a25c727cefbb29891b24163f6fd74
aleaker <alejoakerfeld@gmail.com>
2020-08-13 17:56:16 -0300

    added createQuestions lambda with its own IAM role for dynamodb

---
+++ quiz-api/src/handlers/createQuestion.js
@@ -0,0 +1,28 @@
+import AWS from "aws-sdk";
+import {v4 as uuid} from "uuid";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createQuestion(event, context) {
+
+    const {text, politicianId, source, sourceType} = JSON.parse(event.body);
+
+    const question={
+        questionId: uuid(),
+        text,
+        politicianId,
+        source,
+        sourceType,
+    };
+
+    await dynamodb.put({
+        TableName: "QuestionsTable",
+        Item: question,
+    }).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(question),
+    };
+  };
+  export const handler = createQuestion;
--- quiz-api/src/handlers/getQuestions.js
+++ quiz-api/src/handlers/getQuestions.js
@@ -1,10 +1,10 @@
-async function getAnswers(event, context) {
+async function getQuestions(event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "hi there!" }), //resp body must be a string
   };
 };
 
-export const handler = getAnswers;
+export const handler = getQuestions;
 
 
