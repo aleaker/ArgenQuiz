19c016aeeff152bc48ad0d042233db9379e985ab
aleaker <alejoakerfeld@gmail.com>
2020-08-14 15:23:00 -0300

    getQuestions gets all questions using scan(i should change that for a query)

--- quiz-api/src/handlers/getQuestions.js
+++ quiz-api/src/handlers/getQuestions.js
@@ -1,10 +1,34 @@
+import AWS from "aws-sdk";
+import middy from "@middy/core";
+import createError from "http-errors";
+import httpErrorHandler from "@middy/http-error-handler";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function getQuestions(event, context) {
+  let questions;
+
+  try {
+    const result = await dynamodb
+      .scan({
+        TableName: process.env.QUESTIONS_TABLE_NAME,
+      })
+      .promise();
+
+    questions = result.Items;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError("Something went wrong!");
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "hi there!" }), //resp body must be a string
+    body: JSON.stringify(questions),
   };
-};
-
-export const handler = getQuestions;
-
-
+}
+export const handler = middy(getQuestions)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
