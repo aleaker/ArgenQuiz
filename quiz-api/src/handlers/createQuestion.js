f2f285bd3c4c0012c5d18d2efd6fc05b67f9467b
aleaker <alejoakerfeld@gmail.com>
2020-08-14 14:51:34 -0300

    added middy middleware to createQuestion

--- quiz-api/src/handlers/createQuestion.js
+++ quiz-api/src/handlers/createQuestion.js
@@ -1,28 +1,42 @@
 import AWS from "aws-sdk";
-import {v4 as uuid} from "uuid";
+import middy from "@middy/core";
+import { v4 as uuid } from "uuid";
+import createError from "http-errors";
+import httpErrorHandler from "@middy/http-error-handler";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createQuestion(event, context) {
+  const { text, politicianId, fakesArray, source, sourceType } = event.body; //no need to parse it because of httpJsonBodyParser
 
-    const {text, politicianId, source, sourceType} = JSON.parse(event.body);
+  const question = {
+    questionId: uuid(),
+    text,
+    politicianId,
+    fakesArray,
+    source,
+    sourceType,
+  };
 
-    const question={
-        questionId: uuid(),
-        text,
-        politicianId,
-        source,
-        sourceType,
-    };
-
-    await dynamodb.put({
+  try {
+    await dynamodb
+      .put({
         TableName: process.env.QUESTIONS_TABLE_NAME,
         Item: question,
-    }).promise();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(question),
-    };
+      })
+      .promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(question),
   };
-  export const handler = createQuestion;
+}
+export const handler = middy(createQuestion)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
