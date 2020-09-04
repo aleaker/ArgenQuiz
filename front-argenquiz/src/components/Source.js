e2f325626bd3f4177eb1069364f18b8db286b550
aleaker <alejoakerfeld@gmail.com>
2020-09-04 18:46:48 -0300

    close button for video player

--- front-argenquiz/src/components/Source.js
+++ front-argenquiz/src/components/Source.js
@@ -1,31 +1,41 @@
 import React from "react";
+import videoBorder from "../images/videoBorder.png";
+import cerrarButton from "../images/cerrar.png";
 
-const Source = ({ sourceType, source, videoClosed,closeVideo }) => (
+const Source = ({ sourceType, source, videoClosed, closeVideo }) => (
   <div>
-     {sourceType === "text" ? (
+    {sourceType === "text" ? (
       <p>
         Fuente:
         {
-          <a  href={`https://${source}`} rel="noopener noreferrer" target="_blank">
+          <a
+            href={`https://${source}`}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             Link
           </a>
         }
       </p>
-    ) : ( !videoClosed &&
-    <div className="videoContainer">
-      <iframe
-        className="video"
-        src={`https://www.youtube.com/embed/${source.split("=")[1]}?modestbranding=1&rel=0&fs=0&color=white&controls=0`}
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-        title="video"
-        modestbranding="0"
-
-      />
-<button onClick={closeVideo}>X</button>      
-    </div>
-     )} 
+    ) : (
+      !videoClosed && (
+        <div className="videoContainer">
+          <img className="videoBorder" src={videoBorder} />
+          <iframe
+            className="video"
+            src={`https://www.youtube.com/embed/${
+              source.split("=")[1]
+            }?modestbranding=1&rel=0&fs=0&color=white&controls=0`}
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+            title="video"
+            modestbranding="0"
+          />
+          <img id="closeVideoButton" onClick={closeVideo} src={cerrarButton}/>
+        </div>
+      )
+    )}
   </div>
 );
 
